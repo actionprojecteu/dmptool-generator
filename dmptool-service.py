e10e2c892b2b277614cac5a4484c615bdb58392d
Esteban Gonzalez Guardia <egonzalez@ciclope.info>
2020-02-11 12:24:01 +0100

    Tasks collection name changed in find method

--- dmptool-service.py
+++ dmptool-service.py
@@ -22,13 +22,15 @@ class MyService():
                 logging.info("Processing task with dmp id:"+str(task['dmp']))
                 json_query = {"_id": ObjectId(task['dmp'])};
                 logging.info("Looking for DMP "+str(json_query));
-                dmp = self.db.dmptool.find_one(json_query);
+                dmp = self.db.dmps.find_one(json_query);
                 if (dmp!=None):
                     logging.info("Handling dmp "+str(dmp['_id']));
                     generator = Generator(dmp);
                     generator.generate();
                     logging.info("Updating database");
-                    self.db.tasks.update({"dmp":str(dmp['_id'])},{"status":"done"});
+                    docx_name = "docbook-"+str(dmp['_id'])+".docx";
+                    pdf_name = "docbook-"+str(dmp['_id'])+".pdf";
+                    self.db.tasks.update({"dmp":str(dmp['_id'])},{"status":"done","url_docx":docx_name,"url_pdf":pdf_name});
                 else:
                     logging.info("No DMP found with this id")
 
